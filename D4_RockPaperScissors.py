b07ebd3032d17a16d0d1686fb28fcbb8c97fbdb7
Ruben <rjromo@espol.edu.ec>
2020-11-08 12:13:05 -0500

    Enter

---
+++ D4_RockPaperScissors.py
@@ -0,0 +1,37 @@
+#Project for day 4
+number = input("What do you choose? \nType 0 for ROCK \nType 1 for PAPER \nType 2 for SCISSORS\n")
+
+#rock
+print('''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+)
+
+# Paper
+print(
+    '''
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+'''
+)
+
+# Scissors
+print(
+    '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+)
