f335b35af12a713248bb66c90a51a436ec33bb63
Ruben <rjromo@espol.edu.ec>
2020-12-06 19:24:48 -0500

    Enter

--- D7_.py
+++ D7_.py
@@ -1,1 +1,26 @@
 # Project for day 7
+import random
+
+word_list = ['hello','world','python']
+
+chosen_word = random.choice(word_list)
+
+display = ['_' for letter in chosen_word]
+
+
+print(f'Chosen word: {chosen_word}')
+print(display)
+   
+
+word_length = len(chosen_word)
+
+while True:
+    guess = str(input('Type any letter: ')).lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+            print(display)
+            
+        else:
+            print(display)    
