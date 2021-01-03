b009ece80da3ee1eb4c328f6f6211b3c03e066de
Ruben <rjromo@espol.edu.ec>
2021-01-02 23:21:28 -0500

    Enter

--- D7_Hangman.py
+++ D7_Hangman.py
@@ -1,27 +1,97 @@
 # Project for day 7
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 word_list = ['hello','world','python']
 
 chosen_word = random.choice(word_list)
 
 display = ['_' for letter in chosen_word]
 
-
-print(f'Chosen word: {chosen_word}')
-print(display)
+print ("Welcome to python Hangman \n\n")
+""" print(f'Chosen word: {chosen_word}') """
+print(f"{' '.join(display)}")
    
 
 word_length = len(chosen_word)
 
-while True:
+lives = 6
+
+completedGame = False
+
+
+
+while not completedGame:
     guess = str(input('Type any letter: ')).lower()
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-            print(display)
+            print(f"{' '.join(display)}")
 
-    """  else:
-            print('Wrong')    
-    """
+    if guess not in chosen_word:
+        lives-=1
+        print(stages[lives])    
+   
+        if lives ==0:
+            completedGame = True
+            print ('You lose.')
+            print(f"The chosen word was: {chosen_word}")
+    
